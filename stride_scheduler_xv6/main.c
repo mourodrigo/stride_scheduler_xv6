132902aa695673d96a0c2db6eb16745db0642fcc
mourodrigo <rodrigobt20@gmail.com>
2015-05-27 18:59:46 -0300

    Initial Commit

---
+++ stride_scheduler_xv6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stride_scheduler_xv6
+//
+//  Created by Rodrigo Bueno Tomiosso on 27/05/15.
+//  Copyright (c) 2015 mourodrigo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
