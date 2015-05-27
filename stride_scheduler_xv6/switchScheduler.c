51e970a3e13cdf2e425c2c39bfdbfac4f850deb1
mourodrigo <rodrigobt20@gmail.com>
2015-05-27 20:32:50 -0300

    Alterado para defs e cprintf

--- stride_scheduler_xv6/switchScheduler.c
+++ stride_scheduler_xv6/switchScheduler.c
@@ -27,14 +27,15 @@ main(int argc, char *argv[])
 */
 
 
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
+//#include "types.h"
+//#include "stat.h"
+//#include "user.h"
+#include "defs.h"
 int
 main(int argc, char *argv[])
 {
-    printf(0, "\nHELLO WORLD xv6 0\n");
-    printf(1, "\nHELLO WORLD xv6 1\n");
+    cprintf("\nHELLO WORLD xv6 0\n");
+//    printf(0, "\nHELLO WORLD xv6 0\n");
+//    printf(1, "\nHELLO WORLD xv6 1\n");
     exit();
 }
