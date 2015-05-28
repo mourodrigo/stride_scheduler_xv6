0285a123481191fbe596b3be0fd2e502a5eb61c0
mourodrigo <rodrigobt20@gmail.com>
2015-05-27 21:03:03 -0300

    Alternador de escalonadores

--- stride_scheduler_xv6/switchScheduler.c
+++ stride_scheduler_xv6/switchScheduler.c
@@ -5,11 +5,8 @@
 //  Created by Rodrigo Bueno Tomiosso on 27/05/15.
 //
 //
-/*
-#include "defs.h"
-#include "types.h"
-//#include "stat.h"
-#include "user.h"
+
+ #include "switchScheduler.h"
 
 int
 main(int argc, char *argv[])
@@ -24,13 +21,4 @@ main(int argc, char *argv[])
     
     exit();
 }
-*/
 
-#include "switchScheduler.h"
-int
-main(int argc, char *argv[])
-{
-    printf(0, "\nHELLO WORLD xv6 0\n");
-    printf(1, "\nHELLO WORLD xv6 1\n");
-    exit();
-}
