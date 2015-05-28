e1a262d9f0d0871b12bc5dc83190bee875fecf8d
mourodrigo <rodrigobt20@gmail.com>
2015-05-27 23:12:55 -0300

    comentado log_write outside of trans

--- stride_scheduler_xv6/log.c
+++ stride_scheduler_xv6/log.c
@@ -214,8 +214,9 @@ log_write(struct buf *b)
 
   //if (log.lh.n >= LOGSIZE || log.lh.n >= log.size - 1)
   //  panic("too big a transaction");
-  if (log.outstanding < 1)
-    panic("log_write outside of trans");
+
+    //  if (log.outstanding < 1)
+//    panic("log_write outside of trans");
 
   acquire(&log.lock);
   for (i = 0; i < log.lh.n; i++) {
