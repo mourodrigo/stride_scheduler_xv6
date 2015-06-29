bdaa9429e987a387a20a4bd05d06fef5fbe23f4e
mourodrigo <rodrigobt20@gmail.com>
2015-06-29 11:00:05 -0300

    log min pass

--- stride_scheduler_xv6/proc.c
+++ stride_scheduler_xv6/proc.c
@@ -340,7 +340,8 @@ scheduler(void) //#stride
                 }
                 //        cprintf("pid %d tickets %d/n",p->pid,p->tickets);
             }
-            
+            cprintf("\n\n=================\nProcesso escolhido \n pid %d \n tickets %d \n passos %d \n passada %d \n limite passo %d\n=================\n",minProc->pid,minProc->tickets, minProc->pass , minProc->stride, minProc->pass+minProc->stride);
+
             proc = minProc;
             minProc->pass++;
             switchuvm(minProc);
