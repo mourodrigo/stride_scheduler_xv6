59d3440c2982b480c98e782c8b50722b8a24b224
mourodrigo <rodrigobt20@gmail.com>
2015-06-30 16:11:51 -0300

    get usage removido

--- stride_scheduler_xv6/zombie.c
+++ stride_scheduler_xv6/zombie.c
@@ -27,10 +27,10 @@ void zombie(int pi,int tickets){
 			}
 			
 		}
-	printf(0, "\nturn %d indice %d pid %d tickets %d tempoCPU %d Fatorial %d = %d",turn,pi,getpid(),tickets,getusage(),x,result);
+	printf(0, "\nturn %d indice %d pid %d tickets %d Fatorial %d = %d",turn,pi,getpid(),tickets,x,result);
 //	sleep(SLEEP);
 	}
-	printf(0, "\nturn %d indice %d pid %d tickets %d tempoCPU %d Fatorial %d = %d",turn,pi,getpid(),tickets,getusage(),x,result);
+	printf(0, "\nturn %d indice %d pid %d tickets %d Fatorial %d = %d",turn,pi,getpid(),tickets,x,result);
 //    sleep(SLEEP);
 	exit();
 }
