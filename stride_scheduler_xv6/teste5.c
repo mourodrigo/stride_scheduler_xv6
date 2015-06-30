bf66b47d474aad33d661a53239d16efcedb85ef0
mourodrigo <rodrigobt20@gmail.com>
2015-06-30 20:52:25 -0300

    test changes

--- stride_scheduler_xv6/teste5.c
+++ stride_scheduler_xv6/teste5.c
@@ -76,7 +76,7 @@ int forkTest(int priority,int rodadas){
 int
 main(void)
 {
-    printf(0, "\nEste teste criara 39 processos com prioridades que variam da mais baixa ate mais alta calcularao o fatorial de 0 ate 12 por 4000000 vezes respectivamente. Apos finalizar o calculo sera apresentado o PID do processo com o tempo de execucao.\n\n");
+    printf(0, "\nEste teste criara 16 processos com prioridades baixas calcularao o fatorial de 0 ate 12 por 2400000 vezes respectivamente. Apos finalizar o calculo sera apresentado o PID do processo com o tempo de execucao.\n\n");
     
     int turns = 2400000;
     
